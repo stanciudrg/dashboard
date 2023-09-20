a770ba7376cdc1f11a5873276f23d122e78655bf
stanciudrg <stanciudragos1337@gmail.com>
2023-09-20 18:22:37 +0300

    Design the "New Art" section of the webpage
    Created a scrollable container with 9 paintings, with a custom scrollbar
    style and behavior

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 let shareButton = document.querySelectorAll(".share-btn");
 let downloadButton = document.querySelectorAll(".download-btn");
+let scrollUpButton = document.querySelector(".scroll-up");
+let scrollDownButton = document.querySelector(".scroll-down");
+let scrollableContainer = document.querySelector(".new_art-scrollable");
 
 shareButton.forEach(button => button.addEventListener('mousedown', (e) => {
     e.preventDefault();
@@ -8,3 +11,20 @@ shareButton.forEach(button => button.addEventListener('mousedown', (e) => {
 downloadButton.forEach(button => button.addEventListener('mousedown', (e) => {
     e.preventDefault();
 }));
+
+scrollDownButton.addEventListener("click", (e) => {
+    scrollableContainer.scrollBy({
+        top: 100,
+        left: 0,
+        behavior: "smooth",
+    });
+})
+
+scrollUpButton.addEventListener("click", (e) => {
+
+    scrollableContainer.scrollBy({
+        top: -100,
+        left: 0,
+        behavior: "smooth",
+    });
+})

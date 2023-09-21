9377e16cd7a8982a5198a24b4789f187b0ae801d
stanciudrg <stanciudragos1337@gmail.com>
2023-09-21 15:02:19 +0300

    Create and design custom clear button for the search-bar

--- scripts.js
+++ scripts.js
@@ -2,6 +2,25 @@ let scrollUpButton = document.querySelector(".scroll-up");
 let scrollDownButton = document.querySelector(".scroll-down");
 let scrollableContainer = document.querySelector(".new_art-scrollable");
 let figure = document.querySelectorAll("figure");
+let searchBar = document.querySelector('input[type="search"]');
+let clearSearchBar = document.querySelector(".searchbar_clear-icon");
+let searchBarIcon = document.querySelector('.searchbar-icon')
+
+searchBarIcon.addEventListener("click", (e) => {
+    searchBar.focus();
+})
+
+searchBar.addEventListener("input", (e) => {
+    searchBar.value.length > 0 ? clearSearchBar.style.visibility = 'visible'
+        : clearSearchBar.style.visibility = 'hidden';
+
+})
+
+clearSearchBar.addEventListener("click", (e) => {
+    searchBar.value = "";
+    searchBar.focus();
+    clearSearchBar.style.visibility = 'hidden';
+})
 
 figure.forEach(button => button.addEventListener('mousedown', (e) => {
     e.preventDefault();
